42c67c7d6d6b6223c4062cdd9c9db947b7d05040
Matthew Tanner <imatthewtanner@icloud.com>
2023-10-13 15:32:07 -0500

    Added DetailView

--- Scrumdinger/Models/ScrumsView.swift
+++ Scrumdinger/Models/ScrumsView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
-    
+  
     var body: some View {
-        List(scrums, id: \.title) { scrum in
-            CardView(scrum: scrum)
-                .listRowBackground(scrum.theme.mainColor)
-            ScrumsView(scrums: DailyScrum.sampleData)
+        NavigationStack {
+            List(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
         }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
     }
 }
